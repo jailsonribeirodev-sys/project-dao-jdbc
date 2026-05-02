7956ba008ad3946553166cea291459890976a938
jailson Ribeiro <jailson.ribeirodev@gmail.com>
2026-05-02 15:35:28 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
+	public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	public static LocalDate date = LocalDate.parse("20/02/2010", dtf);
 
 	public static void main(String[] args) {
-	Department obj = new Department(1, "TI");
-	
-	System.out.println(obj);
+		Locale.setDefault(Locale.US);
+		Department obj = new Department(1, "TI");
+
+		Seller seller = new Seller(20, "Raimundo Brito", "raimundo@gmail.com", date, 2214.0, obj);
+		System.out.println(seller);
 	}
 
 }
